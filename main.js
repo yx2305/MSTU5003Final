a423fa928e6fb20b025cf71a6f9599a044aaf5fe
yx2305 <yx2305@tc.columbia.edu>
2016-12-16 19:00:38 -0500

    INITIAL FILES

---
+++ main.js
@@ -0,0 +1,5 @@
+//Rule1: testDate should be no later than college application date(appDate).
+//Rule2: if testNum>=2, time between any two testDate should be >=3 month.
+//Rule3: prepDate1 should be no less than 1 month before testDate1. Same for other prepDate/testDate pair(s) if there is any.
+//Rule4: if user's grade is <12th grade, testNum should be >=2.
+//Question: how to grab these dates, compute them, and output them.
